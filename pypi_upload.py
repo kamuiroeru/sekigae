a1b90f907e11df882795123671fcd0468a16bbff
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2021-01-15 20:52:39 +0900

    Typehint  と Enum を追加
    機能変更はなし。
    testpypiにアップロードする行を追加

--- pypi_upload.py
+++ pypi_upload.py
@@ -3,4 +3,5 @@ from subprocess import run
 
 run(['rm', '-rf', 'sekigae.egg-info', 'dist', 'build'])
 run(['python', 'setup.py', 'sdist', 'bdist_wheel'])
+# run(['twine', 'upload', '--verbose', '--repository', 'testpypi', 'dist/*']) # テスト用環境
 run(['twine', 'upload', '--verbose', '--repository', 'pypi', 'dist/*'])
--- sekigae/sekigae.py
+++ sekigae/sekigae.py
@@ -1,14 +1,24 @@
+from typing import List, Tuple, Iterable
+
 from random import shuffle
 from math import log10, floor
 import csv
+from enum import Enum, auto
+
+Position = List[List[int]]
 
 
 class Sekigae:
-    def _make_table_position(self):
+    class Side(Enum):
+        left = auto()
+        right = auto(
+
+        )
+    def _make_table_position(self) -> Position:
         """
         人数から席配置を示した2次元リストを作る
         """
-        outer_list = []
+        outer_list: Position = []
         people = list(range(1, self.n_people + 1))
         shuffle(people)
         for left in range(0, self.n_people, self.ncol):
@@ -16,7 +26,7 @@ class Sekigae:
 
         return outer_list
 
-    def _reload_params(self, position: [[]], top_label: str):
+    def _reload_params(self, position: Position, top_label: str) -> None:
         self.n_people = sum(len(e) for e in position)
         self.ncol = len(position[0])
         self.max_char_width = floor(log10(self.n_people)) + 1
@@ -26,7 +36,7 @@ class Sekigae:
             ' ' + ' ' * (self.max_char_width - len(str(n))) + str(n) + ' '
         self.make_line = lambda line: '|' + '|'.join([self.table_format(n) for n in line]) + '|'
 
-    def __init__(self, n_people: int, ncol=6, top_label='黒板'):
+    def __init__(self, n_people: int, ncol: int = 6, top_label: str = '黒板') -> None:
         """
         人数から席配置を示した2次元リストを作る
         :param int n_people: 席につく人数
@@ -36,6 +46,7 @@ class Sekigae:
         assert n_people > 0, '人数は 1 以上を指定してください'
         assert ncol > 0, '列数は 1 以上を指定してください'
         assert n_people > ncol, '人数 > 列数 となるように指定してください'
+
         self.n_people = n_people
         self.ncol = ncol
         self.position = self._make_table_position()
@@ -46,13 +57,13 @@ class Sekigae:
             ' ' + ' ' * (self.max_char_width - len(str(n))) + str(n) + ' '
         self.make_line = lambda line: '|' + '|'.join([self.table_format(n) for n in line]) + '|'
 
-    def search(self, n: int) -> (int, int):
+    def search(self, n: int) -> Tuple[int, int]:
         """n 番の位置を探す（脳筋実装）"""
         for lc, line in enumerate(self.position):
             if n in line:
                 return lc, line.index(n)
 
-    def swap(self, a: int, b: int):
+    def swap(self, a: int, b: int) -> None:
         """a と b の配置を入れ替える"""
         assert 0 < a < self.n_people + 1, f'a は 0 < a < {self.n_people + 1} の範囲のみ指定できます'
         assert 0 < b < self.n_people + 1, f'b は 0 < b < {self.n_people + 1} の範囲のみ指定できます'
@@ -61,7 +72,7 @@ class Sekigae:
         self.position[pa[0]][pa[1]] = b
         self.position[pb[0]][pb[1]] = a
 
-    def set(self, x: int, y: int, n: int):
+    def set(self, x: int, y: int, n: int) -> None:
         """
         n を x 行 y 列 にセットする。もともとそこにあった番号が n と入れ替わる
         :param x: 行数
@@ -75,13 +86,13 @@ class Sekigae:
             assert False, 'そこに席はありません'
         self.swap(a, b)
 
-    def show(self, side='left'):
+    def show(self, side: Side = Side.left) -> None:
         """
         席を表示する
         :param str side: "left" or "right" 後ろの席を左寄せにするか右寄せにするか
         :return:
         """
-        assert side == 'left' or side == 'right', 'side は "left" か "right" のどちらかです'
+        assert isinstance(side, self.Side), 'side は Side.left か Side.right のどちらかです'
         print(self.header)
         print(self.line_bar)
         for line in self.position[:-1]:
@@ -90,13 +101,13 @@ class Sekigae:
 
         # 最後の列
         last = self.make_line(self.position[-1])
-        if side == 'right':
+        if side == self.Side.right:
             space = ' ' * (len(self.line_bar) - len(last))
             print(space + last)
         else:
             print(last)
 
-    def to_csv(self, filename='out.csv'):
+    def to_csv(self, filename: str = 'out.csv') -> None:
         """
         席の並びをcsvに吐き出す
         :param str filename: 書き出すCSVファイル名（default: "out.csv"）
@@ -105,7 +116,7 @@ class Sekigae:
             writer = csv.writer(f, lineterminator='\n')
             writer.writerows(self.position)
 
-    def read_csv_iter(self, iterable: iter, top_label: str):
+    def read_csv_iter(self, iterable: Iterable, top_label: str) -> None:
         """
         iterable な Object から CSV を読み込む
         :param iter iterable: iterable
@@ -116,7 +127,7 @@ class Sekigae:
         self.position = [[int(elem) for elem in inner] for inner in reader]
         self._reload_params(self.position, top_label)
 
-    def read_csv(self, filename: str, top_label='黒板'):
+    def read_csv(self, filename: str, top_label: str = '黒板') -> None:
         """
         席の並びが書いてあるcsvを読み込む
         :param str filename: 読み込むCSVファイル名
@@ -125,7 +136,7 @@ class Sekigae:
         with open(filename) as f:
             self.read_csv_iter(f, top_label)
 
-    def show_csv(self):
+    def show_csv(self) -> None:
         """CSV形式で出力する（UNIXパイプライン処理用）"""
         s = '\n'.join([','.join(map(str, inner)) for inner in self.position])
         print(s)
